de99bf7b575f9e7550794854461883a2b06893e2
Neonocyte <neonocyte@yahoo.com>
2017-04-24 16:50:34 -0600

    Basic SignalR and Lobby work

---
+++ AWP_TCG/LobbyHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace AWP_TCG
+{
+    public class LobbyHub : Hub
+    {
+        public void UpdateLobbies()
+        {
+            Clients.All.broadcastMessage();
+        }
+
+        public void AddLobby()
+        {
+
+            UpdateLobbies();
+        }
+    }
+}
